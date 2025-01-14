8996a3a57d8d0b506a6cd1e9d3a9cd6e59b57d72
July-Month <1345464419@qq.com>
2025-01-14 15:44:18 +0800

    feat: addH1

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-const fn = () => 
-  
-  {
-  console.log('这是没有格式化的代码');
+const fn = () => {
+  console.log('这是没有格式化的代码')
 }
 
-fn();
+fn()
